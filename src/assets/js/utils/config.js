a0485fd41e9b17dee84fe22897f2452946e74233
andre <61792917+luuxis@users.noreply.github.com>
2023-10-05 21:43:32 +0200

    Initial commit

---
+++ src/assets/js/utils/config.js
@@ -0,0 +1,51 @@
+/**
+ * @author Luuxis
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
+ */
+
+const pkg = require('../package.json');
+const nodeFetch = require("node-fetch")
+let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
+
+let config = `${url}/launcher/config-launcher/config.json`;
+let news = `${url}/launcher/news-launcher/news.json`;
+
+class Config {
+    GetConfig() {
+        return new Promise((resolve, reject) => {
+            nodeFetch(config).then(async config => {
+                if (config.status === 200) return resolve(config.json());
+                else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
+            }).catch(error => {
+                return reject({ error });
+            })
+        })
+    }
+
+    async getInstanceList() {
+        let urlInstance = `${url}/files`
+        let instances = await nodeFetch(urlInstance).then(res => res.json()).catch(err => err)
+        let instancesList = []
+        instances = Object.entries(instances)
+
+        for (let [name, data] of instances) {
+            let instance = data
+            instance.name = name
+            instancesList.push(instance)
+        }
+        return instancesList
+    }
+
+    async getNews() {
+        return new Promise((resolve, reject) => {
+            nodeFetch(news).then(async config => {
+                if (config.status === 200) return resolve(config.json());
+                else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
+            }).catch(error => {
+                return reject({ error });
+            })
+        })
+    }
+}
+
+export default new Config;
